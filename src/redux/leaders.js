37531c3814df121531102bf9986f473ee0999524
GiorgiaGottifredi <73221723+GiorgiaGottifredi@users.noreply.github.com>
2021-09-24 08:30:40 +0200

    Redux Thunk

--- src/redux/leaders.js
+++ src/redux/leaders.js
@@ -1,18 +1,8 @@
-import {createStore, combineReducers} from 'redux';
-import { Dishes } from './dishes';
-import { Comments } from './comments';
-import { Promotions } from './promotions';
-import { Leaders } from './leaders';
+import { LEADERS } from '../shared/leaders';
 
-export const ConfigureStore = () => {
-    const store = createStore(
-        combineReducers({
-            dishes: Dishes,
-            comments: Comments,
-            promotions: Promotions,
-            leaders: Leaders
-        })
-    );
-
-    return store;
-}
+export const Leaders = (state = LEADERS, action) => {
+    switch (action.type) {
+        default:
+          return state;
+      }
+};
